a7473b606b850367e357d4b6f3ed3b2e372222f3
DESKTOP-24V2TD5\Martiny <mawinClone@gmail.com>
2023-12-01 02:28:59 +0700

    success

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import Transaction from "./components/Transaction";
 import FormComponent from "./components/FormComponent";
 import "./App.css";
-import { useState, useEffect, useReducer } from "react";
+import { useState, useEffect } from "react";
 // import { v4 as uuidv4 } from "uuid";
 import DataContext from "./data/DataContext";
 import ReportComponent from "./components/ReportComponent";
-import { type } from "@testing-library/user-event/dist/type";
-// import { element } from "prop-types";
-// import { element } from "prop-types";
+import { BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+// import { type } from "@testing-library/user-event/dist/type";
+
 
 function App() {
   const design = { color: "darkcyan", textAlign: "center", fontSize: "1.5rem" };
-  const initData = [
-    { id: 1, title: "ค่าเติมเกม", amount: -500 },
-    { id: 2, title: "ถูกหวย", amount: 5000 },
-  ];
-  const [items, setItems] = useState(initData); //useState
+  // const initData = [
+  //   { id: 1, title: "ค่าเติมเกม", amount: -500 },
+  //   { id: 2, title: "ถูกหวย", amount: 5000 },
+  // ];
+  const [items, setItems] = useState([]); //useState
   const [reportIncome, setReportIncome] = useState(0);
   const [reportExpense, setReportExpense] = useState(0);
 
@@ -30,49 +30,49 @@ function App() {
       const amounts = items.map((element) => element.amount);
       // console.log("amounts is ", amounts);
       const income = amounts.filter((element) => element > 0).reduce((total, element) => (total += element), 0);
-      const expense = amounts.filter((element) => element < 0).reduce((total, element) => (total += element), 0);
-      setReportIncome(income);
-      setReportExpense(expense);
+      const expense = amounts.filter((element) => element < 0).reduce((total, element) => (total += element), 0) * -1;
+      setReportIncome(income.toFixed(2));
+      setReportExpense(expense.toFixed(2));
       // console.log("รายได้ ", income);
       // console.log("รายจ่าย ", expense);
-    },
-    [items],
-    reportIncome,
-    reportExpense
+    },[items], reportIncome, reportExpense
   );
 
-  const [showReport, setShowReport] = useState(true);
-  const reducer = (state, action) =>{
-    switch (action.type) {
-      case "Show":
-        return setShowReport(true);
-      case "Hide":
-        return setShowReport(false); 
+  // const [showReport, setShowReport] = useState(true);
+  // const reducer = (state, action) =>{
+  //   switch (action.type) {
+  //     case "Show":
+  //       return setShowReport(true);
+  //     case "Hide":
+  //       return setShowReport(false); 
+  //     default : return state;
+      
   
-    }
-  }
+  //   }
+  // }
 
-  const [result, dispatch] = useReducer(reducer, showReport);
+  // const [result, dispatch] = useReducer(reducer, showReport);
 
   return (
-    <DataContext.Provider
-      value={{
-        name: "Martiny",
-        nickName: "Mawin",
-        expense: reportExpense,
-        income: reportIncome,
-      }}
-    >
+    <DataContext.Provider value={{name: "Martiny",nickName: "Mawin",expense: reportExpense,income: reportIncome,}}>
       <div className="container">
         <h1 style={design}>โปรเเกรมบัญชี รายรับ รายจ่าย </h1>
-        {showReport && <ReportComponent />}
-        <FormComponent onAddItem={onAddNewItem} />
-        {/* ส่งฟังก์ชันให้ FormComponent ในการเรียกย้อนกลับมายัง App */}
 
-        <Transaction items={items} />
-
-        <button onClick={()=>{dispatch({type: "Show"})}}>เเสดง</button>
-        <button onClick={()=>{dispatch({type: "Hide"})}}>ซ่อน</button>
+        <Router>
+          <div>
+            <ul className="horizontal-menu">
+              <li><Link to="/">ข้อมูลบัญชี</Link></li>
+              <li><Link to="/insert">บันทึกข้อมูล</Link></li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/" exact element={<ReportComponent/>}/>  
+            <Route path="/insert" element ={<FormComponent onAddItem={onAddNewItem} />}/>
+          </Routes>
+          <Routes>
+            <Route path="/insert" element ={<Transaction items={items} />}/>
+          </Routes>
+        </Router>
       </div>
     </DataContext.Provider>
   );
