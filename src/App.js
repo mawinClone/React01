17b822fe23fcdb675b548b7b1fd315b1628f798c
DESKTOP-24V2TD5\Martiny <mawinClone@gmail.com>
2023-12-01 00:49:12 +0700

    useReducer for Show, Hide ReportComponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import Transaction from "./components/Transaction";
 import FormComponent from "./components/FormComponent";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 // import { v4 as uuidv4 } from "uuid";
 import DataContext from "./data/DataContext";
 import ReportComponent from "./components/ReportComponent";
+import { type } from "@testing-library/user-event/dist/type";
 // import { element } from "prop-types";
 // import { element } from "prop-types";
 
 function App() {
   const design = { color: "darkcyan", textAlign: "center", fontSize: "1.5rem" };
-
-  // const initData = [
-  //   { id: 1, title: "ค่าเติมเกม", amount: -500 },
-  //   { id: 2, title: "ค่ากาชา", amount: -2000 },
-  //   { id: 3, title: "ค่าโมเดล", amount: -1700 },
-  //   { id: 4, title: "ถูกหวย", amount: 5000 },
-  // ];
-  const [items, setItems] = useState([]); //useState
+  const initData = [
+    { id: 1, title: "ค่าเติมเกม", amount: -500 },
+    { id: 2, title: "ถูกหวย", amount: 5000 },
+  ];
+  const [items, setItems] = useState(initData); //useState
   const [reportIncome, setReportIncome] = useState(0);
   const [reportExpense, setReportExpense] = useState(0);
 
@@ -43,6 +41,19 @@ function App() {
     reportExpense
   );
 
+  const [showReport, setShowReport] = useState(true);
+  const reducer = (state, action) =>{
+    switch (action.type) {
+      case "Show":
+        return setShowReport(true);
+      case "Hide":
+        return setShowReport(false); 
+  
+    }
+  }
+
+  const [result, dispatch] = useReducer(reducer, showReport);
+
   return (
     <DataContext.Provider
       value={{
@@ -53,12 +64,15 @@ function App() {
       }}
     >
       <div className="container">
-        <h1 style={design}>โปรเเกรมบัญชี รายรับ รายจ่าย</h1>
-        <ReportComponent />
+        <h1 style={design}>โปรเเกรมบัญชี รายรับ รายจ่าย </h1>
+        {showReport && <ReportComponent />}
         <FormComponent onAddItem={onAddNewItem} />
         {/* ส่งฟังก์ชันให้ FormComponent ในการเรียกย้อนกลับมายัง App */}
 
         <Transaction items={items} />
+
+        <button onClick={()=>{dispatch({type: "Show"})}}>เเสดง</button>
+        <button onClick={()=>{dispatch({type: "Hide"})}}>ซ่อน</button>
       </div>
     </DataContext.Provider>
   );
--- src/components/ReportComponent.js
+++ src/components/ReportComponent.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import DataContext from "../data/DataContext";
+import './ReportComponent.css'
 
 const ReportComponent = () =>{
 
@@ -7,8 +8,18 @@ const ReportComponent = () =>{
 
     return(
         <div>
-            <p>รายรับ: {income}</p>
-            <p>รายจ่าย: {expense}</p>
+            <h4>ยอดคงเหลือ (บาท)</h4>
+            <h1>฿ {income + expense}</h1>
+            <div className="report-container">
+                <div>
+                    <h4>รายรับ </h4>
+                    <p className="report plus">฿ {income}</p>
+                </div>
+                <div>
+                    <h4>รายจ่าย </h4>
+                    <p className="report minus">฿ {expense}</p>
+                </div>
+            </div>
         </div>
 
     );
